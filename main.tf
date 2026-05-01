4e1b97d51d3f45dd908f61146c1ee67e7b911dbf
coltonhatfield <coltonrhatfield@gmail.com>
2026-04-30 23:27:52 -0400

    added the remnux to terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "proxmox_vm_qemu" "ubuntu_dmz_01" {
   # This forces Ubuntu to wait until the router is fully provisioned
   depends_on = [
@@ -66,12 +67,13 @@ EOF
     bridge = "vmbr1" 
   }
 }
+*/
 
 resource "proxmox_vm_qemu" "opnsense_router" {
   name        = "opnsense-fw-01" 
   target_node = "proxmoxServer"
   
-  clone       = "opnsense-router" 
+  clone       = "opnsense-fw-secure" 
   full_clone  = true
   
   # KEEP THIS. It will work now because the drive won't be deleted.
@@ -119,24 +121,10 @@ resource "proxmox_vm_qemu" "opnsense_router" {
     ignore_changes = [ disks ]
   }
 
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "root" 
-      password = "opnsense" 
-      host     = self.default_ipv4_address
-      timeout  = "5m" 
-    }
-
-    inline = [
-      "pkg install -y os-tailscale",
-      "service tailscaled enable",
-      "service tailscaled start",
-      "tailscale up --authkey=${var.tailscale_auth_key} --advertise-routes=192.168.1.0/24 --accept-routes"
-    ]
-  }
+    
+  
 }
-
+/*
 # Notice I changed the resource name so it doesn't conflict in your mind
 resource "proxmox_vm_qemu" "windows_target_01" {
   name        = "win10-target-01" 
@@ -173,5 +161,87 @@ resource "proxmox_vm_qemu" "windows_target_01" {
       disks,
       efidisk
     ]
+  }
+}
+*/
+
+resource "proxmox_vm_qemu" "remnux_analysis_01" {
+  # Wait for the router to be up so DHCP and routing work for the sandbox
+  depends_on = [
+    proxmox_vm_qemu.opnsense_router
+  ]
+  
+  name        = "remnux-analysis-01"
+  target_node = "proxmoxServer" 
+  
+  # Useful for accessing the REMnux GUI via Proxmox NoVNC
+  vga {
+    type = "std"
+  }
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
+
+  # Point to the REMnux template you just created
+  clone      = "test-remnux-clone" 
+  full_clone = true
+  
+  agent      = 1
+  os_type    = "cloud-init"
+  
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "host"
+  }
+  
+  # REMnux can be a bit heavier due to its toolset, 4GB is a safe baseline
+  memory     = 4096
+  scsihw     = "virtio-scsi-pci"
+  bootdisk   = "scsi0"
+
+  # Cloud-init configuration
+  # If you want to use your custom vendor YAML like on the Ubuntu box, uncomment the line below:
+  # cicustom   = "vendor=local:snippets/vendor.yaml"
+  
+  ciuser     = "colton"
+  cipassword = "TheGoats1234!"
+  ipconfig0  = "ip=dhcp" 
+  
+  # Your existing SSH key for passwordless entry
+  sshkeys    = <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHcX1JuoNjwv7SG+3ykiivVXDQa/j3UMhqy/fw6AUjuoRJegs40KeLLpbLFxOh3/jpyte2UQw5/k7ZQdK3abduSU4tjSSIBy5woj2ZwvjVW8yBa5lMWJfSohIgL3Q7yyynho+YRxp/2oZh32VT3Zj/RB1AhJLcCrd4DAye3v924Sn2ID9gAu6guQ317m6V1kHOxTiN9dbgjmjENckHtMD/LShNTimukST/vdqkake5mJsQC33bE87ed24I40eN2XCq3pDr6xehiLQCERPA4Csny9IRS7IVPAbRvdgHotrSrIBJidEXU62YOerwq6FnFxuiU/BI4IjeuSMoJ2k3mYJq7uAV6Gb9o8DJfJdm2YrVlPIC4i2WeGrj7/TSLuNQNaFcDeBEreheBBtgW9GS63uIJxBD9Ga1JpOEVOV8LFAvP+TSnj4FVbhkDZPeKKIR5xOounXE0Df1vOkv6Rn1uPWWaKPa7w3LNY7XDcBFZv8skTXvLe3vT+3jSFQ6SDUB1Fr3tOTvOA2GUstwaKte9a5O6ZZQPBh8gTMLveurOI4DCxHkcre0FquQNuEYK+zxjQLlp8PpoJSO/O3GOsqGRVHo89+yFQroxx9b5cG39QsB5O6cQypAcilWaSZkKI75k0TqzhdZcEMmP3LGRteOHjqtz1YtErLfD36dKafzSIHW6Q== colto@Colton
+EOF
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          size     = 100 # Adjust to match your REMnux template size
+          storage  = "local-zfs"
+          iothread = true
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local-zfs"
+        }
+      }
+    }
+  }
+
+  # Hooking this into the internal isolated network
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr1" 
+  }
+
+  lifecycle {
+    ignore_changes = [ disks ]
   }
 }
